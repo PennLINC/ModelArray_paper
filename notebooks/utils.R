8de666ba25dac3f71e0faeac2980afed972bc59c
zhao-cy <chenyzh@seas.upenn.edu>
2022-01-18 22:00:59 -0500

    partial R2: add test of missing y

--- notebooks/utils.R
+++ notebooks/utils.R
@@ -4,6 +4,8 @@
 #' @param redmodel The reduced model object returned by e.g. mgcv::gam() with reduced formula
 #' @return A list including: partial R squared, sse for full and reduced models
 partialRsq <- function(fullmodel, redmodel) {
+  # calculating SSE: used observed y (i.e. excluding observations with NA), and fitted values, directly from model object
+  
   sse.full <- sum( (fullmodel$y - fullmodel$fitted.values)^2 )
   sse.red <- sum( (redmodel$y - redmodel$fitted.values)^2 )
   
