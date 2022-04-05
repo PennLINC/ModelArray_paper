9bd58f8290f9ebbe9cffadb07cda05956c061eda
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-04-05 16:58:17 -0400

    add bash command for converting fixels back to mif format

---
+++ notebooks/confixel_to_mifs_pnc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# make sure the conda env is correct:
+# conda activate test_confixel   # <- only do this when running on Chenying's local vmware!
+
+# also temporarily change folder "for_fixelcfestats" as "FDC"
+
+# ++++++++++++++++++++++++++++++++++++
+filename_h5_woext="ltn_FDC_n938_wResults_nfixels-0_20220204-140019"
+# ++++++++++++++++++++++++++++++++++++
+
+cmd="fixelstats_write"
+cmd+=" --index-file index_and_directions_files/index.mif"
+cmd+=" --directions-file index_and_directions_files/directions.mif"
+cmd+=" --cohort-file df_example_n938.csv"
+cmd+=" --relative-root /home/chenying/Desktop/fixel_project/data/data_from_josiane"
+cmd+=" --analysis-name gam_allOutputs"
+cmd+=" --input-hdf5 results/${filename_h5_woext}.h5"
+cmd+=" --output-dir results/${filename_h5_woext}"
+
+echo $cmd
+$cmd
